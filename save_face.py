c2688df078739fa3ebb76c9af5c888c2b5ff2249
wenxiaojie <56551200+wenxiaojie@users.noreply.github.com>
2019-11-05 20:59:49 +0800

    Update save_face.py

--- save_face.py
+++ save_face.py
@@ -1,14 +1,7 @@
 import argparse
-from torchvision import transforms
 import torch
-import torchvision.utils as vutils
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import random
 import helper
-from glob import glob
-import os
 from dcgan_itself import Generator
 
 parser = argparse.ArgumentParser()
@@ -24,7 +17,6 @@ device = torch.device("cuda:0" if(torch.cuda.is_available()) else "cpu")
 # Get the 'params' dictionary from the loaded state_dict.
 params = state_dict['params']
 
-
 for i in range(500):
     # Create the generator network.
     netG = Generator(params).to(device)
@@ -36,7 +28,6 @@ for i in range(500):
     # Get latent vector Z from unit normal distribution.
     noise = torch.randn(int(args.num_output), params['nz'], 1, 1, device=device)
 
-
     def output_fig(images_array, file_name="./results"):
 
         plt.figure(figsize=(6, 6), dpi=100)
@@ -46,14 +37,11 @@ for i in range(500):
 
     # Turn off gradient calculation to speed up the process.
     with torch.no_grad():
-	    # Get generated image from the noise vector using
-	    # the trained generator.
+        # Get generated image from the noise vector using
+        # the trained generator.
         # generated_img = netG(noise).detach().cpu()
-
         generated_img = netG(noise)
-        generated_img = generated_img.permute(0,2,3,1)
 
-
+    generated_img = generated_img.permute(0, 2, 3, 1)
     print(generated_img.shape)  # should be (9, width, height, 3)
-    # width = height = 56
     output_fig(generated_img.cpu().detach().numpy(), file_name="images_jie_final_2/{}_image".format(str.zfill(str(i), 3)))
